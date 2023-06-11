b09aee1c02ce62f242e0693e6e9b99f3f236cd4f
Muhammed <kgokgun94@gmail.com>
2023-06-11 10:53:51 +0300

    ViewBinding added for fragments, button activityes complated.

--- app/src/main/java/com/muhammedkursatgokgun/fragmentskotlin/Fragment1.kt
+++ app/src/main/java/com/muhammedkursatgokgun/fragmentskotlin/Fragment1.kt
@@ -5,24 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.navigation.Navigation
+import com.muhammedkursatgokgun.fragmentskotlin.databinding.Fragment1Binding
+
+private var binding0 : Fragment1Binding? = null
+private val binding get() = binding0!!
 class Fragment1 : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding0 = Fragment1Binding.inflate(inflater,container,false)
+        val view = binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        //return inflater.inflate(R.layout.fragment_1, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+        binding.button.setOnClickListener {
+            val action= Fragment1Directions.actionFragment1ToFragment2()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding0 = null
     }
 }
--- app/src/main/java/com/muhammedkursatgokgun/fragmentskotlin/Fragment2.kt
+++ app/src/main/java/com/muhammedkursatgokgun/fragmentskotlin/Fragment2.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.muhammedkursatgokgun.fragmentskotlin.databinding.Fragment1Binding
+import com.muhammedkursatgokgun.fragmentskotlin.databinding.Fragment2Binding
 
+private var binding0 : Fragment2Binding? = null
+private val binding get() = binding0!!
 class Fragment2 : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +21,24 @@ class Fragment2 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding0 = Fragment2Binding.inflate(inflater,container,false)
+        val view = binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false)
+        //return inflater.inflate(R.layout.fragment_2, container, false)
+        return view
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button2.setOnClickListener {
+            val action = Fragment2Directions.actionFragment2ToFragment1()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding0 = null
+    }
+
 }
