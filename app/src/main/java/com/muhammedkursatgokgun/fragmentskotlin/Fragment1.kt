455a7a7e3f1cbc3a5ff7ac6b90a89a0fd3317fd0
Muhammed <kgokgun94@gmail.com>
2023-06-11 06:54:07 +0300

    Navigation graph added.

--- app/src/main/java/com/muhammedkursatgokgun/fragmentskotlin/Fragment1.kt
+++ app/src/main/java/com/muhammedkursatgokgun/fragmentskotlin/Fragment1.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
 class Fragment1 : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
